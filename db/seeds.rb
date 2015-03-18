9b43fa431c65fcd004a6c66a9395f74231da36e8
Rifqi Thefi Fauzi <rifqi.thefi.fauzi@gmail.com>
2015-03-18 10:11:06 +0700

    seed file final version

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Purging tables...'
+Article.destroy_all
+Comment.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('articles')
+ActiveRecord::Base.connection.reset_pk_sequence!('comments')
+
+puts 'Seeding articles...'
+article_titles = ['Matahari terbenam ',
+  'Bandung berawan',
+  'Keluarga Indonesiaku',
+  'Teriakan sang ibu',
+  'Matahari terbit',
+  'Terngiang dikepalaku',
+  'Jalan setapak',
+  'Dibalik hilangnya uang',
+  'Motor tercepat di RT01',
+  'Kaca tembus pandang',
+  'Gelas anti tumpah',
+  'Pulpen tak habis - habis',
+  'Lilin tahan panas',
+  'Udara yang tidak terlihat']
+article_body = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
+  <p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+  <p>Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. </p>
+  <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. </p>
+  <p>Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci. </p>"
+article_ids = Array.new
+
+article_titles.each do |title|
+  article_ids << Article.create(:title => title, :body => article_body).id
+end
+
+puts 'Seeding comments...'
+comments = ['artikelnya bagus kk',
+  'izin kopas gan',
+  'izin sedot gan',
+  'gan, ane ninggalin jejak dulu',
+  'ane pernah denger tuh',
+  'artikel ini repost',
+  'PM sent gan',
+  'cek PM agan ganteng',
+  'terima kasih mastah',
+  'ajarin ane gan, pliss']
+
+50.times do |index|
+  Comment.create(:article_id => article_ids.sample, :body => comments.sample )
+end
